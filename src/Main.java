a5cab550dfd4ad8d2fda44b66f1b5358f6c6b3a5
P1erreGaultier <puerrepierre@gmail.com>
2018-03-01 20:47:56 +0100

    THAO VA CHEZ LE COIFFEUR

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,46 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
+	
+	
+	
+	
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        
+ 
+    	
 
+    }
+    
+    
+    public void DeplacerVoiture(Entrant ent) {
+    	Map<Integer, ArrayList<Trajet>> res = new HashMap();
+    	for(int i = 0; i<= ent.voitures.size();i++) {
+    		Trajet cible = res.get(i).get(res.get(i).size() - 1); // trajet en cours
+    		ent.voitures.get(i).setCoordonnee(
+    				this.deplacementVers(ent.voitures.get(i).getCoordonnee(), cible.getCoordArrive()));
+    	}
+    	
+    }
+    
+    private Coordonnee deplacementVers(Coordonnee voiture, Coordonnee destination) {
+    	if (voiture.getX() < destination.getX()){
+    		return new Coordonnee(voiture.getX()+1,voiture.getY())
+    	}
+    	if (voiture.getX() > destination.getX()){
+    		return new Coordonnee(voiture.getX()+1,voiture.getY())
+    	}
+    	if (voiture.getY() < destination.getY()){
+    		return new Coordonnee(voiture.getX()+1,voiture.getY())
+    	}
+    	if (voiture.getY() > destination.getY()){
+    		return new Coordonnee(voiture.getX()+1,voiture.getY())
+    	}
+		return destination;
     }
 
 }
+ 
