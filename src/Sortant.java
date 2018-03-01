093cc49d65e4f8d830960b286f85306050ee4a25
Théo Couraud <t.couraud@groupeonepoint.com>
2018-03-01 22:31:10 +0100

    10 point pour gryfondor!

--- src/Sortant.java
+++ src/Sortant.java
@@ -23,9 +23,9 @@ public class Sortant {
             final Integer cle = (Integer) it.next(); // tu peux typer plus finement ici
             final ArrayList<Trajet> valeur = res.get(cle); // tu peux typer plus finement ici
 
-            b = b + cle;
+            b = b + (cle + 1);
             for (final Trajet trajet : valeur) {
-                b = b + " " + trajet.id;
+                b = b + " " + (trajet.id - 1);
             }
             b = b + "\n";
 
