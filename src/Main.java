a0a252863711a44fa77851c814d6140243c30833
Théo Couraud <t.couraud@groupeonepoint.com>
2018-03-01 21:35:18 +0100

    fix main

--- src/Main.java
+++ src/Main.java
@@ -4,57 +4,53 @@ import java.util.Map;
 
 public class Main {
 
+    public static void main(final String[] args) {
 
-    public static void main(String[] args) {
-    	
-    //Init Entrant
-    Entrant ent	= new Entrant();
-    //Init Sortie
-    Map<Voiture, ArrayList<Trajet>> res = new HashMap();
-    for(int i = 0; i <= 10; i++/*ent.NbStep*/) {
-    	//Assignation trajet aux voitures
-    	assignerTrajetVoiture(ent, res);
-    	// maj des positions des voitures
-    	deplacerVoitures(ent);
-    	
-    }
-    // generer le fichier de sortie
-        
-    }
-    
-    public static void deplacerVoitures(Entrant ent) {
-    	Map<Integer, ArrayList<Trajet>> res = new HashMap();
-    	for(int i = 0; i<= ent.voitures.size();i++) {
-    		Trajet cible = res.get(i).get(res.get(i).size() - 1); // trajet en cours
-    		ent.voitures.get(i).setCoordonnee(
-    				deplacementVers(ent.voitures.get(i).getCoordonnee(), cible.getCoordArrive()));
-    	}
-    	
-    }
-    
-   private static void assignerTrajetVoiture(Entrant ent, Map<Voiture, ArrayList<Trajet>> map) {
+        // Init Entrant
+        final Entrant ent = new Entrant();
+        // Init Sortie
+        final Map<Voiture, ArrayList<Trajet>> res = new HashMap();
+        for (int i = 0; i <= 10; i++/* ent.NbStep */) {
+            // Assignation trajet aux voitures
+            assignerTrajetVoiture(ent, res);
+            // maj des positions des voitures
+            deplacerVoitures(ent);
+
+        }
+        // generer le fichier de sortie
 
     }
-   
-   private static boolean checkIfVoitureIsFree(Voiture v, Trajet t) {
-	   return v.coordonnee.equals(t.coordArrive);
-   }
-   
 
-    
-    private static Coordonnee deplacementVers(Coordonnee voiture, Coordonnee destination) {
-    	if (voiture.getX() < destination.getX()){
-    		return new Coordonnee(voiture.getX()+1,voiture.getY());
-    	}
-    	if (voiture.getX() > destination.getX()){
-    		return new Coordonnee(voiture.getX()+1,voiture.getY());
-    	}
-    	if (voiture.getY() < destination.getY()){
-    		return new Coordonnee(voiture.getX()+1,voiture.getY());
-    	}
-    	if (voiture.getY() > destination.getY()){
-    		return new Coordonnee(voiture.getX()+1,voiture.getY());
-    	}	
+    public static void deplacerVoitures(final Entrant ent) {
+        final Map<Integer, ArrayList<Trajet>> res = new HashMap();
+        for (int i = 0; i <= ent.voitures.size(); i++) {
+            final Trajet cible = res.get(i).get(res.get(i).size() - 1); // trajet en cours
+            ent.voitures.get(i).setCoordonnee(
+                    deplacementVers(ent.voitures.get(i).getCoordonnee(), cible.getCoordArrive()));
+        }
+
+    }
+
+    private static void assignerTrajetVoiture(final Entrant ent, final Map<Voiture, ArrayList<Trajet>> map) {
+
+    }
+
+    private static boolean checkIfVoitureIsFree(final Voiture v, final Trajet t) {
+        return v.coordonnee.equals(t.coordArrive);
+    }
+
+    private static Coordonnee deplacementVers(final Coordonnee voiture, final Coordonnee destination) {
+        if (voiture.getX() < destination.getX()) {
+            return new Coordonnee(voiture.getX() + 1, voiture.getY());
+        } else if (voiture.getX() > destination.getX()) {
+            return new Coordonnee(voiture.getX() + 1, voiture.getY());
+        } else if (voiture.getY() < destination.getY()) {
+            return new Coordonnee(voiture.getX() + 1, voiture.getY());
+        } else if (voiture.getY() > destination.getY()) {
+            return new Coordonnee(voiture.getX() + 1, voiture.getY());
+        } else {
+            return null;
+        }
     }
 
 }
